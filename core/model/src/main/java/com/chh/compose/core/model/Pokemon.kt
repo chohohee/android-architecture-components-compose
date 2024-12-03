db0f9fcdb8d37a2cfe41d1d9ee67542b2f473305
chohohee <gone1005@gmail.com>
2024-12-04 01:24:51 +0900

    Add pokemon model

---
+++ core/model/src/main/java/com/chh/compose/core/model/Pokemon.kt
@@ -0,0 +1,16 @@
+package com.chh.compose.core.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pokemon(
+    @SerialName("name") val name: String,
+    @SerialName("url") val url: String
+) {
+
+    private fun getIndex(): String = url.split("/".toRegex()).dropLast(1).last()
+
+    fun getImageUrl(): String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${getIndex()}.png"
+
+}
