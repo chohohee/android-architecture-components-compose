399bf6905810c6fa3085351225e7e63365238382
chohohee <gone1005@gmail.com>
2024-12-10 23:52:32 +0900

    Add palette extension composable

---
+++ core/designsystem/src/main/java/com/chh/compose/core/designsystem/component/DarkRgbColor.kt
@@ -0,0 +1,31 @@
+package com.chh.compose.core.designsystem.component
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import com.kmpalette.palette.graphics.Palette
+
+@Composable
+fun Palette?.darkRgbColor(): State<Color> {
+    val defaultBackground = MaterialTheme.colorScheme.background.toArgb()
+    return remember(this) {
+        derivedStateOf {
+            val darkVibrant = this?.darkVibrantSwatch?.rgb
+            val darkMuted = this?.darkMutedSwatch?.rgb
+            val dominant = this?.dominantSwatch?.rgb
+
+            Color(
+                listOfNotNull(
+                    darkVibrant,
+                    darkMuted,
+                    dominant,
+                    defaultBackground
+                ).first()
+            )
+        }
+    }
+}
---
+++ core/designsystem/src/main/java/com/chh/compose/core/designsystem/component/LightRgbColor.kt
@@ -0,0 +1,31 @@
+package com.chh.compose.core.designsystem.component
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import com.kmpalette.palette.graphics.Palette
+
+@Composable
+fun Palette?.lightRgbColor(): State<Color> {
+    val defaultBackground = MaterialTheme.colorScheme.background.toArgb()
+    return remember(this) {
+        derivedStateOf {
+            val lightVibrant = this?.lightVibrantSwatch?.rgb
+            val lightMuted = this?.lightMutedSwatch?.rgb
+            val dominant = this?.dominantSwatch?.rgb
+
+            Color(
+                listOfNotNull(
+                    lightVibrant,
+                    lightMuted,
+                    dominant,
+                    defaultBackground
+                ).first()
+            )
+        }
+    }
+}
