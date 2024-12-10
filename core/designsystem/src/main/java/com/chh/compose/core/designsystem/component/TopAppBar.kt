0582acc92cb5077acc404baa2b0329f84b6445c2
chohohee <gone1005@gmail.com>
2024-12-10 23:11:46 +0900

    Add common app bar

---
+++ core/designsystem/src/main/java/com/chh/compose/core/designsystem/component/TopAppBar.kt
@@ -0,0 +1,47 @@
+package com.chh.compose.core.designsystem.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chh.compose.core.designsystem.theme.AACComposeTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    onNavigationClick: () -> Unit
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = title) },
+        modifier = modifier,
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                    contentDescription = "arrow back",
+                    tint = MaterialTheme.colorScheme.onSurface,
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun MainTopBarPreview() {
+    AACComposeTheme {
+        TopAppBar(
+            "title",
+            onNavigationClick = {}
+        )
+    }
+}
