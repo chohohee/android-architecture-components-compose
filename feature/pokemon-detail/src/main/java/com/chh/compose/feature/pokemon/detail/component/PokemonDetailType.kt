40f4d15834f35392cd4ad4d09a5d2776d63fee4e
chohohee <gone1005@gmail.com>
2024-12-12 10:58:19 +0900

    Add type preview

--- feature/pokemon-detail/src/main/java/com/chh/compose/feature/pokemon/detail/component/PokemonDetailType.kt
+++ feature/pokemon-detail/src/main/java/com/chh/compose/feature/pokemon/detail/component/PokemonDetailType.kt
@@ -1,8 +1,16 @@
 package com.chh.compose.feature.pokemon.detail.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chh.compose.core.designsystem.theme.AACComposeTheme
 import com.chh.compose.core.model.PokemonInfo
+import com.chh.compose.core.model.PokemonInfo.TypeResponse
+import com.chh.compose.core.model.Type
 
 @Composable
 fun PokemonDetailType(
@@ -15,5 +23,30 @@ fun PokemonDetailType(
             type = item.type.name,
             modifier = modifier
         )
+    }
+}
+
+@Preview(
+    widthDp = 400,
+    heightDp = 80
+)
+@Composable
+private fun PokemonDetailTypePreview() {
+    AACComposeTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            PokemonDetailType(
+                pokemonInfo = PokemonInfo(
+                    id = 1,
+                    name = "bulbasaur",
+                    height = 7,
+                    weight = 69,
+                    types = listOf(TypeResponse(Type("grass")))
+                )
+            )
+        }
     }
 }
