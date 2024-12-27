d0c7f270c1a75ce1134899980a83e72de4fae4ba
chohohee <gone1005@gmail.com>
2024-12-27 09:52:21 +0900

    Update theme background

--- feature/main/src/main/java/com/chh/compose/feature/main/component/MainNaviHost.kt
+++ feature/main/src/main/java/com/chh/compose/feature/main/component/MainNaviHost.kt
@@ -2,9 +2,9 @@ package com.chh.compose.feature.main.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import com.chh.compose.feature.type.navigation.typeNavGraph
 import com.chh.compose.feature.pokemon.navigation.pokemonNavGraph
@@ -21,7 +21,7 @@ internal fun MainNaviHost(
     NavHost(
         navController = navigator.navController,
         startDestination = navigator.startDestination,
-        modifier = modifier.background(color = Color.White)
+        modifier = modifier.background(color = MaterialTheme.colorScheme.background)
     ) {
 
         pokemonNavGraph(
