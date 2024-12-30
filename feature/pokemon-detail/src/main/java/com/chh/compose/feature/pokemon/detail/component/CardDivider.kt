dc8bdb0946920cf5afb40051ecfdae47042f9deb
chohohee <gone1005@gmail.com>
2024-12-30 10:09:12 +0900

    Update theme background

--- feature/pokemon-detail/src/main/java/com/chh/compose/feature/pokemon/detail/component/CardDivider.kt
+++ feature/pokemon-detail/src/main/java/com/chh/compose/feature/pokemon/detail/component/CardDivider.kt
@@ -2,14 +2,13 @@ package com.chh.compose.feature.pokemon.detail.component
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chh.compose.core.designsystem.theme.AACComposeTheme
-import com.chh.compose.core.model.Pokemon
 
 @Composable
 fun CardDivider(
@@ -18,12 +17,13 @@ fun CardDivider(
 ) {
     val endOffset = arrayOf((-5).dp, 85.dp)
     val startOffset = if (index % 2 == 0) endOffset else arrayOf(endOffset[1], endOffset[0])
+    val backgroundColor = MaterialTheme.colorScheme.background
 
     Canvas(
         modifier = modifier.aspectRatio(1f)
     ) {
         drawLine(
-            color = Color.White,
+            color = backgroundColor,
             start = Offset(startOffset[0].toPx(), endOffset[0].toPx()),
             end = Offset(startOffset[1].toPx(), endOffset[1].toPx()),
             strokeWidth = 10.dp.toPx()
